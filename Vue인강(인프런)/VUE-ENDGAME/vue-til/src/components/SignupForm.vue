c706c5bdba2e35ce433a75166067261d038b981a
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-05-20 18:18:53 +0900

    signupForm 수정

--- Vue인강(인프런)/VUE-ENDGAME/vue-til/src/components/SignupForm.vue
+++ Vue인강(인프런)/VUE-ENDGAME/vue-til/src/components/SignupForm.vue
@@ -17,12 +17,18 @@
         <button type="submit" class="btn">회원 가입</button>
       </form>
       <p class="log">{{ logMessage }}</p>
+
+      <!-- id가 형식에 안맞을 경우 화면에 메세지 띄워줘 봄. -->
+      <p v-if="!isUsernameValid && username !== ''" class="alert">
+        {{ username }}는 사용할 수 없는 id입니다.
+      </p>
     </div>
   </div>
 </template>
 
 <script>
 import { registerUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
 
 export default {
   data() {
@@ -35,23 +41,49 @@ export default {
       logMessage: '',
     };
   },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
   methods: {
     async submitForm() {
-      const userData = {
-        username: this.username,
-        password: this.password,
-        nickname: this.nickname,
-      };
-      const { data } = await registerUser(userData);
-      console.log(data.username);
-      this.logMessage = `${data.username} 님이 가입되었습니다`;
-      this.initForm();
+      try {
+        console.log('폼 제출');
+        const userData = {
+          username: this.username,
+          password: this.password,
+          nickname: this.nickname,
+        };
+        const { data } = await registerUser(userData); // 여기에는 api에서 작성했던 url은 적을 필요 없이 함수의 인자로 받은 data만 적어주면 된다.
+        console.log(data.user.username);
+        this.logMessage = `${data.user.username} 님이 가입되었습니다.`;
+        this.initForm(); // input창을 clear해줌.
+      } catch (error) {
+        // console.log(error.response);
+        this.logMessage = `${this.username}은 이미 가입된 id입니다. ${error}`;
+      }
     },
     initForm() {
-      this.username = '';
+      this.username = ''; // ''대신 Null로 해도 되긴하다. 하지만 견고한 코드작성을 위해서는 타입시스템을 이해하면서 타입을 정의해주는게 버그를 줄일 수 있는 방법이다.
       this.password = '';
       this.nickname = '';
     },
+    //   const userData = {
+    //     username: this.username,
+    //     password: this.password,
+    //     nickname: this.nickname,
+    //   };
+    //   const { data } = await registerUser(userData);
+    //   console.log(data.username);
+    //   this.logMessage = `${data.username} 님이 가입되었습니다`;
+    //   this.initForm();
+    // },
+    // initForm() {
+    //   this.username = '';
+    //   this.password = '';
+    //   this.nickname = '';
+    // },
   },
 };
 </script>
