23cf334e9041997e4081a30e59cec9566b48d411
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-05-27 10:22:01 +0900

    104강까지 수강(학습 노트 데이터 삭제)

--- Vue인강(인프런)/VUE-ENDGAME/vue-til/src/components/posts/PostListItem.vue
+++ Vue인강(인프런)/VUE-ENDGAME/vue-til/src/components/posts/PostListItem.vue
@@ -8,16 +8,30 @@
     </div>
     <div class="post-time">
       {{ postItem.createdAt }}
+      <i class="icon ion-md-create"></i>
+      <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
   </li>
 </template>
 
 <script>
+import { deletePost } from '@/api/posts';
+
 export default {
   props: {
     postItem: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    async deleteItem() {
+      if (confirm('You want to delete ie?')) {
+        await deletePost(this.postItem._id);
+        // refresh를 MainPage로 올려보내준다.
+        this.$emit('refresh');
+        console.log('deleted');
+      }
     },
   },
 };
